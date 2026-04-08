aaef408c04aeb6e4dd5bf549a941c04178a87fed
Patrick Solamillo <patricksolamillo@proton.me>
2026-04-08 14:00:26 -0400

    set up routingg and app shell layout
    - create 3 routes missionlist, missionfilter. mission details/id with root redirecting to mission list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Missionlist } from './missionlist/missionlist';
+import { Missionfilter } from './missionfilter/missionfilter';
+import { Missiondetails } from './missiondetails/missiondetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: '', redirectTo: 'missionlist', pathMatch: 'full'},
+  {path: 'missionlist', title: 'Mission List', component: Missionlist},
+  {path: 'missionfilter', title: 'Mission Filter', component: Missionfilter},
+  {path: 'missiondetails/:id', title: 'Mission Details', component: Missiondetails},
+  {path: '**', redirectTo: 'missionlist'}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('101464667-lab-test2-comp3133');
+  protected readonly title = signal('COMP3133 Lab Test 2a - SpaceX Mission Tracker');
 }
